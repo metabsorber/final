4665ef8fa3fe37887dbc7f75e54225441580851f
Serj Minichev <serjminichev@gmail.com>
2024-01-20 16:08:53 +0300

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by Сергей Миничев on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
